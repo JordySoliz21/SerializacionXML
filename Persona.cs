f123cd805893b954e2d95a9a0ab3769923d62334
LABORATORIO 3 <LABORATORIO 3@DESKTOP-NVKFFL7>
2024-04-03 07:13:25 -0600

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SerializacionXML
+{
+    // Clase para serializar/deserializar
+    [Serializable]
+    public class Persona
+    {
+        public string PrimerNombre { get; set; } 
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public int Edad {  get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Crear una persona para serializar
+
+using SerializacionXML;
+
+Persona persona = new Persona()
+{
+    PrimerNombre = "Jordy",
+    SegundoNombre = "Alberto",
+    PrimerApellido = "Valverde",
+    SegundoApellido = "Soliz",
+    Edad = 18
+};
+
+// Usar factory de serializacion XML
+
+ISerializerFactory xmlSerializerFactory = new XmlSerializerFactory();
+ObjectSerializer xmlObjectSerializer = new ObjectSerializer(xmlSerializerFactory);
+xmlObjectSerializer.Serialize(persona, "persona.xml");
+
+// Deserializar persona desde XML
+
+Persona personaFromXML = xmlObjectSerializer.Deserialize<Persona>("persona.xml");
+
+Console.WriteLine(
+    "Persona desde XML: ," + 
+    "Nombre = {0} {1} ," + 
+    "Apellido = {2} {3} ," + 
+    "Edad = {4}" , personaFromXML.PrimerNombre, personaFromXML.SegundoNombre,
+    personaFromXML.PrimerApellido, personaFromXML.SegundoNombre, personaFromXML.Edad);
